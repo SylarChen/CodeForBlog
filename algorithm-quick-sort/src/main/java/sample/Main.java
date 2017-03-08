944f87ac66e40ee197fb98fb12c1006408bd55eb
Sylar Chen <chenheng415@163.com>
2017-03-08 21:07:42 +0800

    t

--- algorithm-quick-sort/src/main/java/sample/Main.java
+++ algorithm-quick-sort/src/main/java/sample/Main.java
@@ -6,12 +6,16 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
     	
-    	int[] arr = new int[]{123,313,43,134,43,6456,32,367,0,12,424,54,32,453,46,31,435,6570,12,332};
-    	System.out.println(Arrays.toString(arr));
-    	QuickSort.sort(arr);
+		int[] arr = new int[20];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (Math.random() * 100);
+		}
+		int[] arr2 = Arrays.copyOf(arr, arr.length);
+		
     	System.out.println(Arrays.toString(arr));
     	
-    	int[] arr2 = new int[]{123,313,43,134,43,6456,32,367,0,12,424,54,32,453,46,31,435,6570,12,332};
+    	QuickSort.sort(arr);
+    	System.out.println(Arrays.toString(arr));
     	Arrays.sort(arr2);
     	System.out.println(Arrays.toString(arr2));
     }
