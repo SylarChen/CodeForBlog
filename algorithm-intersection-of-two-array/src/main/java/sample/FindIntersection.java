8a730226a621819043ad31186d52e70fec9d1b3f
Chen, Heng (Sylar,XS-SH) heng.chen@hp.com <heng.chen@hp.com>
2017-03-17 16:00:45 +0800

    temp

--- algorithm-intersection-of-two-array/src/main/java/sample/FindIntersection.java
+++ algorithm-intersection-of-two-array/src/main/java/sample/FindIntersection.java
@@ -1,19 +1,19 @@
 package sample;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindIntersection {
 
 	// 给定两个数组，找出他们的交集
-	public static Integer[] find(int[] arr1, int[] arr2) {
+	public static Integer[] find1(int[] arr1, int[] arr2) {
 		Arrays.sort(arr1);
 		Arrays.sort(arr2);
 		System.out.println(Arrays.toString(arr1));
 		System.out.println(Arrays.toString(arr2));
 		
-		List<Integer> resultList = new LinkedList<Integer>();
+		Set<Integer> resultSet = new HashSet<Integer>();
 
 		int indexOfArr1 = 0;
 		int indexOfArr2 = 0;
@@ -21,7 +21,7 @@ public class FindIntersection {
 			int benchMarkInArr1 = arr1[indexOfArr1];
 			for ( ; indexOfArr2 < arr2.length; indexOfArr2++) {
 				if(benchMarkInArr1 == arr2[indexOfArr2]){
-					resultList.add(benchMarkInArr1);
+					resultSet.add(benchMarkInArr1);
 				}else if(benchMarkInArr1 < arr2[indexOfArr2]){
 					break;
 				}
@@ -35,15 +35,22 @@ public class FindIntersection {
 			int benchMarkInArr2 = arr2[indexOfArr2];
 			for ( ; indexOfArr1 < arr1.length; indexOfArr1++) {
 				if(benchMarkInArr2 == arr1[indexOfArr1]){
-					resultList.add(benchMarkInArr2);
+					resultSet.add(benchMarkInArr2);
 				}else if(benchMarkInArr2 < arr1[indexOfArr1]){
 					break;
 				}
 			}
 		}
 
-		return resultList.toArray(new Integer[resultList.size()]);
-
+		return resultSet.toArray(new Integer[resultSet.size()]);
 	}
 
+	public static Integer[] find2(int[] arr1, int[] arr2) {
+		
+		Set<Integer> set1 = new HashSet<Integer>();
+		
+		
+
+		return null;
+	}
 }
--- algorithm-intersection-of-two-array/src/main/java/sample/Main.java
+++ algorithm-intersection-of-two-array/src/main/java/sample/Main.java
@@ -8,15 +8,17 @@ public class Main {
     	
 		int[] arr1 = new int[20];
 		for (int i = 0; i < arr1.length; i++) {
-			arr1[i] = (int) (Math.random() * 100);
+//			arr1[i] = (int) (Math.random() * 100);
+			arr1[i] = 22225;
 		}
 		
 		int[] arr2 = new int[20];
 		for (int i = 0; i < arr2.length; i++) {
-			arr2[i] = (int) (Math.random() * 100);
+//			arr2[i] = (int) (Math.random() * 100);
+			arr2[i] = 22225;
 		}
 		
-		Integer[] result = FindIntersection.find(arr1, arr2);
+		Integer[] result = FindIntersection.find1(arr1, arr2);
 		System.out.println(Arrays.toString(result));
 		
     }
