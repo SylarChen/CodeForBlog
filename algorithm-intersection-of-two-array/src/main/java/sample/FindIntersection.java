688e4bf7ac37ef37c0c9ffc1b01a9a65c42580fe
Sylar Chen <chenheng415@163.com>
2017-03-18 12:22:38 +0800

    temp

--- algorithm-intersection-of-two-array/src/main/java/sample/FindIntersection.java
+++ algorithm-intersection-of-two-array/src/main/java/sample/FindIntersection.java
@@ -1,23 +1,26 @@
 package sample;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+//给定两个数组，找出他们的交集
 public class FindIntersection {
-
-	// 给定两个数组，找出他们的交集
+	
+	//对两个数组排序，然后交叉查找
 	public static Integer[] find1(int[] arr1, int[] arr2) {
+		//排序
 		Arrays.sort(arr1);
 		Arrays.sort(arr2);
-		System.out.println(Arrays.toString(arr1));
-		System.out.println(Arrays.toString(arr2));
 		
 		Set<Integer> resultSet = new HashSet<Integer>();
 
 		int indexOfArr1 = 0;
 		int indexOfArr2 = 0;
 		while (indexOfArr1 < arr1.length && indexOfArr2 < arr2.length) {
+			//从数组1中取出一个元素，逐一和数组2的元素比较，直到数组2的元素大于这个取出的元素
 			int benchMarkInArr1 = arr1[indexOfArr1];
 			for ( ; indexOfArr2 < arr2.length; indexOfArr2++) {
 				if(benchMarkInArr1 == arr2[indexOfArr2]){
@@ -26,12 +29,13 @@ public class FindIntersection {
 					break;
 				}
 			}
-			//数组1坐标后移
+			//数组1坐标后移一位
 			indexOfArr1 = indexOfArr1 + 1;
 			if(indexOfArr1 >= arr1.length || indexOfArr2 >= arr2.length){
-				//有数组已经遍历到头，那不再可能有更多的交集元素
+				//任意数组已经遍历到头，那不再可能有更多的交集元素
 				break;
 			}
+			//从数组2中取出一个元素，逐一和数组1的元素比较，直到数组1的元素大于这个取出的元素
 			int benchMarkInArr2 = arr2[indexOfArr2];
 			for ( ; indexOfArr1 < arr1.length; indexOfArr1++) {
 				if(benchMarkInArr2 == arr1[indexOfArr1]){
@@ -45,12 +49,54 @@ public class FindIntersection {
 		return resultSet.toArray(new Integer[resultSet.size()]);
 	}
 
+	//使用HashSet
 	public static Integer[] find2(int[] arr1, int[] arr2) {
+		Set<Integer> resultSet = new HashSet<Integer>();
 		
+		//HashSet存取的时间复杂度都为O(1) 
+		Set<Integer> set = new HashSet<Integer>();
+		for(int num : arr1){
+			set.add(num);
+		}
+		for(int num : arr2){
+			if(set.contains(num)){
+				resultSet.add(num);
+			}
+		}
+		
+		return resultSet.toArray(new Integer[resultSet.size()]);
+	}
+	
+	//合并成一个数组，排序后遍历，相邻相同即为交集元素
+	public static Integer[] find3(int[] arr1, int[] arr2) {
+		Set<Integer> resultSet = new HashSet<Integer>();
+		
+		//给两个数组去重，合并
 		Set<Integer> set1 = new HashSet<Integer>();
+		for(int num : arr1){
+			set1.add(num);
+		}
+		Set<Integer> set2 = new HashSet<Integer>();
+		for(int num : arr2){
+			set2.add(num);
+		}
+		List<Integer> list = new ArrayList<Integer>();
+		list.addAll(set1);
+		list.addAll(set2);
+		Integer[] newArr = list.toArray(new Integer[list.size()]);
 		
+		//排序
+		Arrays.sort(newArr);
 		
-
-		return null;
+		//遍历一遍,相邻相同即是交集
+		Integer previous = null;
+		for(Integer current : newArr){
+			if(previous == current){
+				resultSet.add(previous);
+			}
+			previous = current;
+		}
+		
+		return resultSet.toArray(new Integer[resultSet.size()]);
 	}
 }
--- algorithm-intersection-of-two-array/src/main/java/sample/Main.java
+++ algorithm-intersection-of-two-array/src/main/java/sample/Main.java
@@ -6,20 +6,23 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
     	
-		int[] arr1 = new int[20];
+		int[] arr1 = new int[100];
 		for (int i = 0; i < arr1.length; i++) {
-//			arr1[i] = (int) (Math.random() * 100);
-			arr1[i] = 22225;
+			arr1[i] = (int) (Math.random() * 100);
 		}
 		
-		int[] arr2 = new int[20];
+		int[] arr2 = new int[100];
 		for (int i = 0; i < arr2.length; i++) {
-//			arr2[i] = (int) (Math.random() * 100);
-			arr2[i] = 22225;
+			arr2[i] = (int) (Math.random() * 100);
 		}
 		
-		Integer[] result = FindIntersection.find1(arr1, arr2);
-		System.out.println(Arrays.toString(result));
+		Integer[] result1 = FindIntersection.find1(Arrays.copyOf(arr1, arr1.length), Arrays.copyOf(arr2, arr2.length));
+		System.out.println(Arrays.toString(result1));
 		
+		Integer[] result2 = FindIntersection.find2(Arrays.copyOf(arr1, arr1.length), Arrays.copyOf(arr2, arr2.length));
+		System.out.println(Arrays.toString(result2));
+		
+		Integer[] result3 = FindIntersection.find3(Arrays.copyOf(arr1, arr1.length), Arrays.copyOf(arr2, arr2.length));
+		System.out.println(Arrays.toString(result3));
     }
 }
